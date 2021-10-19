b7845c28b401552df1d1108387b1c222a13d065c
wandol <wandol@kakao.com>
2021-10-20 02:29:48 +0900

    22221

--- src/main/java/com/example/testjava8/commuting/Temp3Main.java
+++ src/main/java/com/example/testjava8/commuting/Temp3Main.java
@@ -12,6 +12,7 @@ import java.net.URLEncoder;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -27,10 +28,10 @@ public class Temp3Main {
         //  경기도 버스 좌표로 naver api를 이용하여 각 정류소간의 소요시간 산정.
         //  https://map.naver.com/v5/api/transit/directions/point-to-point?start=126.9328667,37.3727833,name=곡란초등학교&goal=126.9316167,37.3751167,name=개나리후문&mode=TIME&lang=ko
 //        List<BusNodeVO> sBusNodeList = utils.readCsv(BusNodeVO.class, "csvs/METRO-SEB-NODE-INFO.csv");
-        List<BusNodeVO> sBusNodeList = utils.readCsv(BusNodeVO.class, "bus/METRO-GGDB-NODE-INFO.csv");
+        List<BusNodeVO> sBusNodeList = utils.readCsv(BusNodeVO.class, "bus/METRO-ICNB-NODE-INFO.csv");
 
         Map<String,List<BusNodeVO>> map = sBusNodeList.stream().collect(Collectors.groupingBy(BusNodeVO::getBus_route_id));
-
+        AtomicInteger index = new AtomicInteger();
         List<BusNodeVO> resultList = new LinkedList<>();
         map.forEach((s, busNodeVOS) -> {
             IntStream.range(0, busNodeVOS.size()).forEach( i -> {
@@ -42,11 +43,16 @@ public class Temp3Main {
                         url.append("&goal="+ URLEncoder.encode(busNodeVOS.get(i+1).getGps_x()+","+busNodeVOS.get(i+1).getGps_y()+",name=" + busNodeVOS.get(i+1).getSt_nm(),"utf-8"));
                         url.append("&mode=TIME&lang=ko");
                         System.out.println(url.toString());
-                        if(i%1000 == 0) System.out.println(i+"/"+sBusNodeList.size());
                         Map<String,String> returnMap = utils.getTakeMinData(url.toString());
-                        busNodeVOS.get(i).setDist_next_st(returnMap.get("distance"));
-                        busNodeVOS.get(i).setTime_next_st(returnMap.get("duration"));
-                        resultList.add(busNodeVOS.get(i));
+                        if(returnMap.size() > 0){
+                            busNodeVOS.get(i).setDist_next_st(returnMap.get("distance"));
+                            busNodeVOS.get(i).setTime_next_st(returnMap.get("duration"));
+                            resultList.add(busNodeVOS.get(i));
+                        }else{
+                            busNodeVOS.get(i).setDist_next_st("500");
+                            busNodeVOS.get(i).setTime_next_st("2");
+                            resultList.add(busNodeVOS.get(i));
+                        }
                     }else{
                         resultList.add(busNodeVOS.get(i));
                     }
@@ -58,12 +64,14 @@ public class Temp3Main {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                index.getAndIncrement();
             });
+            if(index.get()%1000 == 0) System.out.println(index.get() +"/"+sBusNodeList.size());
         });
 
 
         //  지하철 - 버스 정류장 연결 csv만들기.
-        CSVWriter writer = new CSVWriter(new FileWriter("d:/METRO-GGDB-NODE-INFO-TOBE.csv"));
+        CSVWriter writer = new CSVWriter(new FileWriter("d:/METRO-ICNB-NODE-INFO-TOBE.csv"));
         String[] cate = {"bus_route_id","bus_route_nm","ars_id","st_id","st_nm","gps_x","gps_y",
                 "pos_x","pos_y","seq","dist_next_st","time_next_st","trn_st_id","city_nm"};
         writer.writeNext(cate);
